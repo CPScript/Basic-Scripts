9aa623dd0ac7f77d25fb4aa61d28fc3b9910bb9b
Chai <83523587+CPScript@users.noreply.github.com>
2023-04-11 11:29:30 -0400

    Create Wipe divice.ps1

---
+++ Windows Scripts/Wipe divice.ps1
@@ -0,0 +1,24 @@
+### Powershell ###
+##################
+
+# PowerShell script to execute the wipe action on Windows devices is given below
+$namespaceName = "root\cimv2\mdm\dmmap"
+$className = "MDM_RemoteWipe"
+$methodName = "doWipeMethod"
+
+$session = New-CimSession
+
+$params = New-Object Microsoft.Management.Infrastructure.CimMethodParametersCollection
+$param = [Microsoft.Management.Infrastructure.CimMethodParameter]::Create("param", "", "String", "In")
+$params.Add($param)
+
+try
+{
+    $instance = Get-CimInstance -Namespace $namespaceName -ClassName $className -Filter "ParentID='./Vendor/MSFT' and InstanceID='RemoteWipe'"
+    $session.InvokeMethod($namespaceName, $instance, $methodName, $params)
+}
+catch [Exception]
+{
+    write-host $_ | out-string
+}
+# The doWipeMethod triggers the device to start the remote wipe.

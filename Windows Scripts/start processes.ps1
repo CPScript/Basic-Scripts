6976f80702d3261f923f768981b44a36decffe50
Chai <83523587+CPScript@users.noreply.github.com>
2023-04-11 11:54:21 -0400

    Create start processes.ps1

---
+++ Windows Scripts/start processes.ps1
@@ -0,0 +1,6 @@
+### Powershell ###
+##################
+
+# The following script starts the specified processes on Windows devices. You can use this script to automate the task of starting multiple processes on a device.
+# Replace "FileAddress" with the file path of the process you want to start in the device’s background. 
+Start-Process "FileAddress" 
